2453eb5306e281f9a11a29ff8ceb9fd5495fb6df
jamesweber <weber99@purdue.edu>
2018-04-12 00:36:09 -0400

    Commit 4/12/18 12:30 AM

---
+++ source/ahb_lite_slave_interface.sv
@@ -0,0 +1,21 @@
+module ahb_lite_slave_interface (
+	input wire HCLK,
+	input wire HRESETn,
+	input wire HSELx,
+	input wire [31:0] HADDR,
+	input wire HWRITE,
+	input wire [2:0] HSIZE,
+	input wire [2:0] HBURST,
+	input wire [3:0] HPROT,
+	input wire [1:0] HTRANS,
+	input wire HMASTLOCK,
+	input wire HREADY,
+	input wire [128:0] HWDATA,
+	output wire HREADYOUT,
+	output wire HRESP,
+	output wire [31:0] HRDATA
+);
+
+	
+
+endmodule // ahb_lite_slave_interface
