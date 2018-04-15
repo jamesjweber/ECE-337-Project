51e64b057c181bb558bc768673d837a819a0681a
jamesweber <weber99@purdue.edu>
2018-04-15 02:43:02 -0400

    Commit 4/15/18 2:45 AM
    Added error checking

--- Source/AHB_Lite/Slave_Interface/ahb_lite_slave_interface.sv
+++ Source/AHB_Lite/Slave_Interface/ahb_lite_slave_interface.sv
@@ -8,18 +8,27 @@ module ahb_lite_slave_interface (
 	input wire [2:0] HBURST,
 	input wire [3:0] HPROT,
 	input wire [1:0] HTRANS,
-	input wire HMASTLOCK,
 	input wire HREADY,
 	input wire [128:0] HWDATA,
-	output wire HREADYOUT,
-	output wire HRESP,
-	output wire [31:0] HRDATA
+	output reg HREADYOUT,
+	output reg HRESP,
+	output reg [31:0] HRDATA
 );
 
 // Internal Signals
 output wire [127:0] SWDATA;
+output wire sizeControlError;
 
-size_control SC(HWDATA, HSIZE, SWDATA);
+output wire error;
+output wire ready;
+
+// Combining Signals
+assign error = sizeControlError; // & future error signals
+assign ready = 1'b1; // for right now
+
+// Internal Blocks
+transfer_response TR(HCLK, HRESETn, ready, error, HREADY, HRESP);
+size_control SC(HWDATA, HSIZE, SWDATA, sizeControlError);
 
 always_ff @ (posedge HCLK, negedge HRESETn) begin
 	// Reset stuff
