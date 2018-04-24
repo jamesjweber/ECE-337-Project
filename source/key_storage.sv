a96e82738eedc8efe969cb7edc32a2748aab50e4
John Kansky <kansky@purdue.edu>
2018-04-23 20:48:56 -0400

    Something

--- source/key_storage.sv
+++ source/key_storage.sv
@@ -4,7 +4,7 @@ module key_storage
 	input wire [127:0] in,
 	output wire [127:0] roundKey
 );
-
+reg [127:0] currentData;
 always_ff @ (posedge clk)
 begin
 	currentData <= in;
--- source/prekey_storage.sv
+++ source/prekey_storage.sv
@@ -2,10 +2,12 @@ module prekey_storage
 (
 	input wire clk,
 	input wire rst,
-	input wire [127:0] in,
-	output wire [255:0] gen_preKeys,
-	output wire [255:0] pre_roundKeys,
-)
+	input wire [127:0] keyIn,
+	input wire [31:0] in,
+	output wire [127:0] gen_preKeys,
+	output wire [127:0] pre_roundKeys,
+	output wire [127:0] pre_froundKeys
+);
 
 reg [31:0] pKeyMinus1;
 reg [31:0] pKeyMinus2;
@@ -18,20 +20,20 @@ reg [31:0] pKeyMinus8;
 
 always_ff @ (posedge clk)
 begin
-	if(rst == 0)
+	if(rst == 1)
 	begin
-		pKeyMinus8 = {1b'1,in[0:30]};
-		pKeyMinus7 = in[31:62];	
-		pKeyMinus6 = in[63:94];	
-		pKeyMinus5 = in[95:126];	
-		pKeyMinus4 = {in[127],31'h0};
-		pKeyMinus3 = 32'h0;
-		pKeyMInus2 = 32'h0;
-		pKeyMinus1 = 32'h0;
+		pKeyMinus8 <= {1'b1,keyIn[30:0]};
+		pKeyMinus7 <= keyIn[62:31];	
+		pKeyMinus6 <= keyIn[94:63];	
+		pKeyMinus5 <= keyIn[126:95];	
+		pKeyMinus4 <= {keyIn[127],31'h0};
+		pKeyMinus3 <= 32'h0;
+		pKeyMinus2 <= 32'h0;
+		pKeyMinus1 <= 32'h0;
 	end
 	else
 	begin
-		pKeyMinus1 <= in[31:0];
+		pKeyMinus1 <= in;
 		pKeyMinus2 <= pKeyMinus1;
 		pKeyMinus3 <= pKeyMinus2;
 		pKeyMinus4 <= pKeyMinus3;
@@ -43,6 +45,7 @@ begin
 end
 
 assign gen_preKeys = {pKeyMinus8, pKeyMinus5, pKeyMinus3, pKeyMinus1};
-assign pre_roundKeys = {pKeyMinus1, pKeyMinus2, pKeyMinus3, pKeyMinus4};
+assign pre_froundKeys = {pKeyMinus1, pKeyMinus2, pKeyMinus3, pKeyMinus4};
+assign pre_roundKeys = {pKeyMinus5, pKeyMinus6, pKeyMinus7, pKeyMinus8};
 
 endmodule
