bed545c0cc9dda19bcd75c8f934e9f2010614139
John Kansky <kansky@purdue.edu>
2018-04-23 20:00:39 -0400

    added files

--- source/round_start.sv
+++ source/round_start.sv
@@ -1,19 +1,24 @@
 module round_start
-
-{
+(
 	input wire clk,
 	input wire init,
 	input wire [128:0] freshData,
 	input wire [128:0] priorRound,
 	output wire [128:0] dataOut
-}
+);
 
 reg [128:0] currentData;
+reg [128:0] dataMinus1;
+reg [128:0] dataMinus2;
+reg [128:0] dataMinus3;
 wire dataSelect;
 
 always_ff @ (posedge clk)
 begin
-	currentData = dataSelect;
+	currentData <= dataMinus1;
+	dataMinus1 <= dataMinus2;
+	dataMinus2 <= dataMinus3;
+	dataMinus3 <= dataSelect;
 end
 
 assign dataSelect = init ? freshData: priorRound;
