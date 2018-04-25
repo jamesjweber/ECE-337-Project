59494ff49ca55ba209cdec103473d14fe57680e4
GotCubes <spencerdeak@gmail.com>
2018-04-25 17:49:35 -0400

    Update flex_counter.sv

--- source/flex_counter.sv
+++ source/flex_counter.sv
@@ -1,60 +1,37 @@
-// $Id: $
-// File name:   flex_counter.sv
-// Created:     2/1/2018
-// Author:      James Weber
-// Lab Section: 337-04
-// Version:     1.0  Initial Design Entry
-// Description: Flexible Counter
-
 module flex_counter
 #(
-	parameter NUM_CNT_BITS = 4
+	parameter NUM_CNT_BITS = 8
 )
 (
 	input wire clk,
 	input wire n_rst,
 	input wire clear,
 	input wire count_enable,
-	input reg [NUM_CNT_BITS-1:0] rollover_val,
-	output reg [NUM_CNT_BITS-1:0] count_out,
-	output reg rollover_flag
+	input wire [NUM_CNT_BITS - 1:0]rollover_val,
+	output wire [NUM_CNT_BITS - 1:0]count_out
 );
-	// Register Declarations
-	reg [NUM_CNT_BITS-1:0] next_count_out;
-	reg [NUM_CNT_BITS-1:0] r_flag;
 
-	assign count_out = next_count_out;
-	assign rollover_flag = r_flag;
+	reg [NUM_CNT_BITS - 1:0]cur_count = 0;
+	reg [NUM_CNT_BITS - 1:0]nxt_count = 0;
 
-	always @ (posedge clk, negedge n_rst) begin
-		if (~n_rst) begin
-			// if n_rst (active low), then reset registers
-			next_count_out <= '0;
-			r_flag <= 0;
-		end else begin
-			if (clear) begin
-				// if clear set, then clear count and flag
-				next_count_out <= '0;
-				r_flag <= 0;
-			end else begin
-				if (count_enable) begin
-					if (rollover_flag) begin
-						// if flag is set, reset count and flag
-						next_count_out <= 1;
-						r_flag <= 0;
-					end else begin
-						// if flag isn't set increase value of count
-						next_count_out <= count_out + 1;
-						// if the count hits the rollover value, set flag
-						if (next_count_out+1 == rollover_val) begin
-							r_flag <= 1;
-						end
-					end
-				end else begin
-					r_flag <= rollover_flag; // maintains flag value
-				end
-			end	
-		end
+	always_ff @ (posedge clk, negedge n_rst)
+	begin : FF_LOGIC
+		if(n_rst == 1'b0)
+			cur_count <= 0;
+		else
+			cur_count <= nxt_count;
 	end
+
+	always_comb
+	begin : NXT_LOGIC
+		nxt_count = count_out;
+
+		if(clear == 1'b1)
+			nxt_count = 0;
+		else if(count_enable == 1'b1)
+			nxt_count = count_out + 1;
+	end
+
+	assign count_out = cur_count;
 
 endmodule
