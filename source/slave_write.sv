a9fa3e0ffdfd19cbfab7cd831a27d85c4ee1ec76
James Weber <weber99@purdue.edu>
2018-04-26 02:02:14 -0400

    ahb slave works

--- source/slave_write.sv
+++ source/slave_write.sv
@@ -18,40 +18,38 @@ module slave_write (
 );
 
 // User Defined Types
-typedef enum bit [4:0] {S1,S2,S3,S4,S5,S6,S7,S8,S9,S10,S11,S12,S13,S14,S15,S16,S17,S18,S19,S20,S21,S22,S23,S24,S25,S26,S27,S28,S29,S30,S31,S32} stateType;
+typedef enum bit [1:0] {S1,S2,S3,S4} stateType;
 
 // Internal Signals
 stateType state;
+stateType nextState;
 reg [31:0] 	prev_HADDR;
 reg [127:0] prev_key;
 reg [127:0] prev_nonce;
 reg [31:0] 	prev_dest;
 reg [127:0] prev_text;
 
-always_ff @ (posedge HCLK, posedge HRESETn) begin
-  if (valid) begin // If selected and not being reset
+always_ff @ (posedge HCLK, negedge HRESETn) begin
+  if (HRESETn == 1'b0) begin // If selected and not being reset
     prev_HADDR <= HADDR;
     prev_key   <= key;
     prev_nonce <= nonce;
     prev_dest  <= destination;
     prev_text  <= plain_text;
+    state <= nextState;
   end else begin // Else if being reset and/or not currently selected
-    //state <= S1;
-    // write_ready = 1'b1; // AHB Protocol: During reset all slaves must ensure that HREADYOUT is HIGH.
-    prev_HADDR <= 32'b0;
+    state <= S1;
     prev_key 	 <= 128'b0;
     prev_nonce <= 128'b0;
     prev_dest  <= 32'b0;
     prev_text  <= 128'b0;
   end
-end
 
-always_ff @ (HCLK, HBURST, HTRANS) begin
-	convert({HBURST, HTRANS}, state); // Set next state
 end
 
 always_comb begin
 
+	nextState = state;
   key = prev_key;
   nonce = prev_nonce;
   destination = prev_dest;
@@ -59,15 +57,24 @@ always_comb begin
   write_error = 1'b0;
   write_ready = 1'b1;
   write_out = 1'b0;
-
-  // See slave_write.md (README) for description of all states
-
-  case (state)
+  
+  if (HTRANS == 2'b0) begin
+  	nextState = S1;
+  end else if (HTRANS == 2'b1) begin
+  	nextState = S2;
+  end else if (HTRANS == 2'b10) begin
+    nextState = S3;
+ 	end else begin
+ 		nextState = S4;
+ 	end
+ 	
+  casez (state)
 
     S1:
     begin
       // HBURST: SINGLE, HTRANS: IDLE
       // Do not read data in IDLE state
+      // nextState = S1;
     end
 
     S2:
@@ -83,12 +90,12 @@ always_comb begin
       // HBURST: SINGLE, HTRANS: NONSEQ
       // Single burst write
       write_error = 1'b0;
-      key = prev_key;
+      /* key = prev_key;
       nonce = prev_nonce;
       destination = prev_dest;
-      plain_text = prev_text;
+      plain_text = prev_text; */
 
-      if (HREADY == 1'b1) begin
+      // if (HREADY == 1'b1) begin
         // If ready write to address
         // Choose to not use address 0x00 so data would not be accidentally overwritten.
         if (prev_HADDR[7:0] == 8'h04) begin
@@ -152,7 +159,7 @@ always_comb begin
           $display("INVALID ADDRESS: %h", prev_HADDR[7:0]);
           write_error = 1'b1;
         end
-      end
+      //end
 
     end
 
@@ -163,265 +170,13 @@ always_comb begin
       write_error = 1'b1;
     end
 
-    S5:
-    begin
-      // HBURST: INCR, HTRANS: IDLE
-      // Do not write data in IDLE state
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S6:
-    begin
-      // HBURST: INCR, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S7:
-    begin
-      // HBURST: INCR, HTRANS: NONSEQ
-      // Beginning of INCR burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S8:
-    begin
-      // HBURST: INCR, HTRANS: SEQ
-      // Continuation of burst from state S7
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S9:
-    begin
-      // HBURST: WRAP4, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S10:
-    begin
-      // HBURST: WRAP4, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S11:
-    begin
-      // HBURST: WRAP4, HTRANS: NONSEQ
-      // Beginning of WRAP4 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S12:
-    begin
-      // HBURST: WRAP4, HTRANS: SEQ
-      // Continuation of burst from state S11.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S13:
-    begin
-      // HBURST: INCR4, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S14:
-    begin
-      // HBURST: INCR4, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S15:
-    begin
-      // HBURST: INCR4, HTRANS: NONSEQ
-      // Beginning of INCR4 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S16:
-    begin
-      // HBURST: INCR4, HTRANS: SEQ
-      // Continuation of burst from state S15.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S17:
-    begin
-      // HBURST: WRAP8, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S18:
-    begin
-      // HBURST: WRAP8, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-
-
-    end
-
-    S19:
-    begin
-      // HBURST: WRAP8, HTRANS: NONSEQ
-      // Beginning of WRAP8 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S20:
-    begin
-      // HBURST: WRAP8, HTRANS: SEQ
-      // Continuation of burst from state S20.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S21:
-    begin
-      // HBURST: INCR8, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S22:
-    begin
-      // HBURST: INCR8, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S23:
-    begin
-      // HBURST: INCR8, HTRANS: NONSEQ
-      // Beginning of INCR8 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S24:
-    begin
-      // HBURST: INCR8, HTRANS: SEQ
-      // Continuation of burst from state S23.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S25:
-    begin
-      // HBURST: WRAP16, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S26:
-    begin
-      // HBURST: WRAP16, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S27:
-    begin
-      // HBURST: WRAP16, HTRANS: NONSEQ
-      // Beginning of WRAP16 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S28:
-    begin
-      // HBURST: WRAP16, HTRANS: SEQ
-      // Continuation of burst from state S26.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S29:
-    begin
-      // HBURST: INCR16, HTRANS: IDLE
-      // Do not write data in IDLE state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S30:
-    begin
-      // HBURST: INCR16, HTRANS: BUSY
-      // Do not write data in BUSY state.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S31:
-    begin
-      // HBURST: INCR16, HTRANS: NONSEQ
-      // Beginning of WRAP16 burst.
-
-      // Not supported
-      write_error = 1'b1;
-    end
-
-    S32:
-    begin
-      // HBURST: INCR16, HTRANS: SEQ
-      // Continuation of burst from state 31.
-
-      // Not supported
-      write_error = 1'b1;
-    end
+    default: nextState = S1;
 
   endcase
 
 end
 
-task convert;
+/* task convert;
 input reg [4:0] numeric_state;
 output stateType nstate;
 begin
@@ -460,7 +215,7 @@ begin
     5'b11111: nstate = S32;
     default: nstate = S1;
 	endcase
-end
-endtask
+end 
+endtask */
 
 endmodule // slave_write
