c4dfcdc4e5103ea8c90187253f8501b1ba8d71c4
GotCubes <spencerdeak@gmail.com>
2018-04-26 02:02:33 -0400

    Update AES_block.sv

--- source/AES_block.sv
+++ source/AES_block.sv
@@ -84,28 +84,5 @@ ahb_lite_slave_interface si(
 	.plain_text(plain_text),
 	.write_out(write_out)
 );
-
-/*
-module ahb_lite_slave_interface (
-	input wire HCLK,
-	input wire HRESETn,
-	input wire HSELx,
-	input wire [31:0] HADDR,
-	input wire HWRITE,
-	input wire [2:0] HSIZE,
-	input wire [2:0] HBURST,
-	input wire [1:0] HTRANS,
-	input wire HREADY,
-	input wire [31:0] HWDATA,
-	input wire fifo_full,
-	output reg HREADYOUT,
-	output reg HRESP,
-	output reg [31:0] HRDATA,
-	output reg [127:0] key,
-	output reg [127:0] nonce,
-	output reg [31:0] destination,
-	output reg [127:0] plain_text,
-	output reg write_out
-);
- */
+	
 endmodule
