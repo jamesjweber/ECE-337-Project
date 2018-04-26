63c7137ad7ac8f1a4462c8f306029e398761ddbf
jamesweber <weber99@purdue.edu>
2018-04-26 02:25:21 -0400

    slave write

--- source/tb_ahb_lite_slave_interface.sv
+++ source/tb_ahb_lite_slave_interface.sv
@@ -11,19 +11,12 @@ localparam 	AHB_BUS_SIZE 	= 32;
 // Shared Test Variables
 reg tb_HCLK;
 reg tb_HRESETn;
-reg tb_HREADY;
-reg tb_HRESP;
-reg [AHB_BUS_SIZE - 1:0] tb_HRDATA;
-reg [AHB_BUS_SIZE - 1:0] tb_destination;
-reg tb_dest_updated;
-reg [(4 * AHB_BUS_SIZE) - 1:0] tb_encr_text;
-reg tb_text_rcvd;
+reg tb_HSELx;
 reg [AHB_BUS_SIZE - 1:0] tb_HADDR;
 reg tb_HWRITE;
 reg [2:0] tb_HSIZE;
 reg [2:0] tb_HBURST;
-reg [1:0] tb_HTRANS;
-reg [AHB_BUS_SIZE - 1:0] tb_HWDATA;
+reg 
 
 // Test bench signals
 int tb_test_num;
@@ -37,6 +30,28 @@ begin
   tb_HCLK = 1'b1;
   #(CLK_PERIOD/2.0);
 end
+
+ahb_lite_slave_interface DUT(
+	.HCLK(tb_HCLK),
+	.HRESETn(tb_HRESETn),
+	.HSELx(tb_HSELx),
+  .HADDR(tb_HADDR),
+  .HWRITE(tb_HWRITE),
+	input wire [2:0] HSIZE,
+	input wire [2:0] HBURST,
+	input wire [1:0] HTRANS,
+	input wire HREADY,
+	input wire [31:0] HWDATA,
+	input wire fifo_full,
+	output reg HREADYOUT,
+	output reg HRESP,
+	output reg [31:0] HRDATA,
+	output reg [127:0] key,
+	output reg [127:0] nonce,
+	output reg [31:0] destination,
+	output reg [127:0] plain_text,
+	output reg write_out
+);
 
 ahb_lite_slave_interface DUT(
   .HCLK(tb_HCLK),
