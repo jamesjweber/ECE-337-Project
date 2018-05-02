70f6d47fd665cb1008d3996a3173ea93d399e319
GotCubes <spencerdeak@gmail.com>
2018-05-02 14:37:26 -0400

    Added comments.

--- source/flex_counter.sv
+++ source/flex_counter.sv
@@ -1,3 +1,5 @@
+// Based on the module programmed in lab.
+
 module flex_counter
 (
 	input wire clk,
@@ -8,8 +10,8 @@ module flex_counter
 	output wire [7:0] count_out
 );
 
-	reg [7:0] cur_count = 0;
-	reg [7:0] nxt_count = 0;
+	reg [7:0] cur_count = 0; // Temporary count.
+	reg [7:0] nxt_count = 0; // Next count.
 
 	always_ff @ (posedge clk, negedge n_rst)
 	begin : FF_LOGIC
@@ -21,14 +23,16 @@ module flex_counter
 
 	always_comb
 	begin : NXT_LOGIC
+		// Default values.
 		nxt_count = count_out;
-
-		if(clear == 1'b1)
+		
+		if(clear == 1'b1) // Clear the count.
 			nxt_count = 0;
-		else if(count_enable == 1'b1)
+		else if(count_enable == 1'b1) // Increment the count.
 			nxt_count = count_out + 1;
 	end
 
+	// Assign output.
 	assign count_out = cur_count;
 
 endmodule
