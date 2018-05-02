c20df36b45e8ddaa78e7137104e18f3db2caf273
Lyusternik <kansky@purdue.edu>
2018-05-02 16:00:31 -0400

    added comments

--- source/prekey_gen.sv
+++ source/prekey_gen.sv
@@ -11,11 +11,10 @@ module prekey_gen
 	wire [31:0] mid1;
 	wire [31:0] mid2;
 
-	flex_counter counter (.clk(clk), .n_rst(~rst), .clear(rst), .count_enable(1'b1), .rollover_val(8'd132), .count_out(count));
+	flex_counter counter (.clk(clk), .n_rst(~rst), .clear(rst), .count_enable(1'b1), .rollover_val(8'd132), .count_out(count)); //132 prekeys in Serpent
 
-	assign ct = {24'b0, count};
-	assign hold = preKeyIn[31:0] ^ preKeyIn[63:32] ^ preKeyIn[95:64] ^ preKeyIn[127:96] ^ 32'h9e3779b9 ^ ct;
-	assign ct = {24'b0, count};
-	assign prKey = (hold << 11) | (hold >> 21);
+	assign ct = {24'b0, count}; //pad count out to 32 bits
+	assign hold = preKeyIn[31:0] ^ preKeyIn[63:32] ^ preKeyIn[95:64] ^ preKeyIn[127:96] ^ 32'h9e3779b9 ^ ct; //Generate next prekey...
+	assign prKey = (hold << 11) | (hold >> 21);//...and rotate it
   
 endmodule
