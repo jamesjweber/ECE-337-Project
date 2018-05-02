77c8fbdcf7c882daab84e4e64eb4fe60aef1e662
Lyusternik <kansky@purdue.edu>
2018-05-02 15:40:10 -0400

    Added comments

--- source/key_block.sv
+++ source/key_block.sv
@@ -2,22 +2,22 @@ module key_block
 (
 	input wire clk,
 	input wire rst,
-	input wire keyLock,
+	input wire keyLock, //when high, load new keys
 	input wire [2:0] select,
 	input wire [127:0] in,
 	output wire [127:0] roundKey,
 	output wire [127:0] froundKey
 );
 
-wire [31:0] generatorToStorage_con;
-wire [127:0] storageToGenerator_con;
-wire [127:0] storageToNS_box;
-wire [127:0] storageToFS_box;
-wire [127:0] fSboxToStorage;
+	wire [31:0] generatorToStorage_con; //output of prekey generator to prekey storage
+	wire [127:0] storageToGenerator_con; //output of prekey storage to prekey generator
+	wire [127:0] storageToNS_box; //output of prekey storage to 'normal' round key s-box
+	wire [127:0] storageToFS_box; //output of prekey storage to 'final' round key s-box
+	wire [127:0] fSboxToStorage; //out of s-box to key storage
 wire [127:0] nSboxToStorage;
 wire [2:0] fselect;
 
-assign fselect = select - 1;
+assign fselect = select - 1; //final round key S-box is always one box behind
 
 key_storage ksn(.clk(clk), .keyLock(keyLock), .in(nSboxToStorage), .roundKey(roundKey));
 key_storage ksf(.clk(clk), .keyLock(keyLock), .in(fSboxToStorage), .roundKey(froundKey));
